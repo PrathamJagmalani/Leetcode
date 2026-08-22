d73b37c0e925e90a2a0321187e6a2846a021e2a5
PrathamJagmalani <prathamjagmalani1@gmail.com>
2026-08-22 17:14:44 +0530

    Time: 3 ms (73.67%) | Memory: 47.8 MB (34.66%) - LeetSync

---
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+         List<Integer> result = new ArrayList<>();
+        int i = num.length - 1;
+
+        // Loop as long as there are digits in the array OR k still has value
+        while (i >= 0 || k > 0) {
+            // Add the current array digit to k if we haven't run out of digits
+            if (i >= 0) {
+                k += num[i];
+                i--;
+            }
+            
+            // Extract the last digit of the combined sum and add it to our list
+            result.add(k % 10);
+            
+            // Carry over the remaining value to the next position
+            k /= 10;
+        }
+
+        // Because we added digits from right to left, reverse the final list
+        Collections.reverse(result);
+        return result;
+    }
+}
