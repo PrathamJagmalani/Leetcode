63b14fd5c1b94c9994684a45e2951981a91948a1
PrathamJagmalani <prathamjagmalani1@gmail.com>
2026-08-26 18:55:12 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (71.49%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -1,28 +1,25 @@
 class Solution {
     public List<Integer> luckyNumbers(int[][] matrix) {
-       
-        int m=matrix.length;
-        int n=matrix[0].length;
-        int[] min=new int[m];
-        int[] max=new int[n];
-
-        Arrays.fill(min,Integer.MAX_VALUE);
-        Arrays.fill(max,Integer.MIN_VALUE);
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                min[i]=Math.min(matrix[i][j],min[i]);
-                max[j]=Math.max(matrix[i][j],max[j]);
+        List<Integer> list=new ArrayList<>();
+        for(int i=0;i<matrix.length;i++){
+            int minidx=min(matrix[i]);
+            if(max(minidx,i,matrix.length,matrix))list.add(matrix[i][minidx]);
+        }
+        return list;
+    }
+    public boolean max(int minidx,int j,int n,int [][]matrix){
+        for(int i=0;i<n;i++){
+            if(matrix[j][minidx]<matrix[i][minidx])return false;
+        }
+        return true;
+    }
+    public int min(int[] arr){
+        int minidx=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<arr[minidx]){
+                minidx=i;
             }
         }
-
-        List<Integer> list=new ArrayList<>();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-               if(matrix[i][j]==min[i] && matrix[i][j]==max[j])
-               list.add(matrix[i][j]);
-            }
-          }
-   return list;
-}
+        return minidx;
+    }
 }
