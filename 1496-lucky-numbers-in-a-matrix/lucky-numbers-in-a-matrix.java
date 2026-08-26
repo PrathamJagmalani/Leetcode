205e040a1bde8b9c565098ad4e3520fc60e964a0
PrathamJagmalani <prathamjagmalani1@gmail.com>
2026-08-26 18:51:19 +0530

    Time: 3 ms (45.17%) | Memory: 47.1 MB (51.28%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -1,21 +1,24 @@
 class Solution {
     public List<Integer> luckyNumbers(int[][] matrix) {
-        List<Integer> list=new ArrayList<>();
-        int[] min=new int[matrix.length];
-        int[] max=new int[matrix[0].length];
+       
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int[] min=new int[m];
+        int[] max=new int[n];
 
         Arrays.fill(min,Integer.MAX_VALUE);
         Arrays.fill(max,Integer.MIN_VALUE);
 
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 min[i]=Math.min(matrix[i][j],min[i]);
                 max[j]=Math.max(matrix[i][j],max[j]);
             }
         }
 
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
+        List<Integer> list=new ArrayList<>();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                if(matrix[i][j]==min[i] && matrix[i][j]==max[j])
                list.add(matrix[i][j]);
             }
