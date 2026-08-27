2e2fe20e544e80d09471bda89d6595730f69c45f
PrathamJagmalani <prathamjagmalani1@gmail.com>
2026-08-28 01:26:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (22.25%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -10,7 +10,7 @@ class Solution {
     private void rotate(int[][] mat) {
         int n = mat.length;
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
+            for (int j = i ; j < n; j++) {
                 int temp = mat[i][j];
                 mat[i][j] = mat[j][i];
                 mat[j][i] = temp;
