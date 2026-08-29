edbee1a9007255baa464c1865fcf376dcd239391
PrathamJagmalani <prathamjagmalani1@gmail.com>
2026-08-29 15:45:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (62.39%) - LeetSync

--- 1378-cells-with-odd-values-in-a-matrix/cells-with-odd-values-in-a-matrix.java
+++ 1378-cells-with-odd-values-in-a-matrix/cells-with-odd-values-in-a-matrix.java
@@ -1,30 +1,28 @@
 class Solution {
     public int oddCells(int m, int n, int[][] indices) {
-        int [][]arr=new int[m][n];
-        for(int i=0;i<indices.length;i++){
-            int j=0;
-            int k=indices[i][j];
-             for(int y=0;y<n;y++){
-                arr[k][y]++;
-           }        
+        int[] rows = new int[m];
+        int[] cols = new int[n];
+
+        for (int[] indice : indices) {
+            rows[indice[0]]++;
+            cols[indice[1]]++;
         }
 
-        for(int i=0;i<indices.length;i++){
-            int j=1;
-            int k=indices[i][j];
-             for(int y=0;y<m;y++){
-                arr[y][k]++;
-           }        
-        }
-            
-         int c=0;
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[i].length;j++){
-                if(arr[i][j]%2!=0){
-                    c++;
-                }
+        int oddRows = 0, oddCols = 0;
+
+        for (int num : rows) {
+            if ((num & 1) == 1) {
+                oddRows++;
             }
-           }        
-        return c;
+        }
+
+        for (int num : cols) {
+            if ((num & 1) == 1) {
+                oddCols++;
+            }
+        }
+
+        return oddRows * (n - oddCols)
+             + oddCols * (m - oddRows);
     }
 }
