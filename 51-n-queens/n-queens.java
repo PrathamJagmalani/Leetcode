075c99e6021eb74f4c64120d114ae18796c0ab51
PrathamJagmalani <prathamjagmalani1@gmail.com>
2026-09-14 18:09:36 +0530

    Time: 4 ms (25.90%) | Memory: 46.5 MB (63.83%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,49 @@
+class Solution {
+    public static boolean isSafe(String board[][],int row,int col,int n){
+        for(int i=0;i<n;i++){
+            if(board[row][i]=="Q") return false;
+        }
+        
+        for(int i=0;i<n;i++){
+            if(board[i][col]=="Q") return false;
+        }
+
+        for(int i=row,j=col;i>=0 && j>=0;i--,j--){
+            if(board[i][j]=="Q") return false;
+        }
+
+        for(int i=row,j=col;i>=0 && j<n;i--,j++){
+            if(board[i][j]=="Q") return false;
+        }
+
+        return true;
+    }
+    
+    void nQueens(String board[][],int row,int n,List<List<String>> ans){
+       if (row == n) {
+             List<String> currentBoard = new ArrayList<>();
+            for (String[] r : board) {
+                currentBoard.add(String.join("", r));
+            }
+            ans.add(currentBoard);
+            return; 
+        }
+        for(int i=0;i<n;i++){
+        if(isSafe(board,row,i,n)){
+           board[row][i]="Q";
+           nQueens(board,row+1,n,ans);
+           board[row][i]=".";
+        }
+      }
+    }
+     
+    public List<List<String>> solveNQueens(int n) {
+        String[][] board=new String[n][n];
+        for (String[] row : board) {
+            Arrays.fill(row, "."); 
+        }
+        List<List<String>> ans=new ArrayList<>();
+    nQueens(board,0, n, ans);
+    return ans;
+    }
+}
